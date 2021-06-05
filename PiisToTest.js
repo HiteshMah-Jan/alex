3440421fc1bf1708d467405210f891264bf1b2b5
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-06-05 16:45:06 +0530

    Create PiisToTest.js

---
+++ PiisToTest.js
@@ -0,0 +1,17 @@
+const DataElements = {
+  "salary" : "100",
+  "credit_card": "200",
+  "firstName": "Hitesh",
+  "lastName" : "M",
+  "email" : "YOYOYO",
+  "secret" : "AccountPassword",
+  "address" : "Street 2, zip 3",
+  "shipping_address" : "A",
+  "ratings" : "5-star",
+  "employee_id": "1234",
+  "religion" : "Sneaky",
+  "FatherPhoneNumber": "abcd",
+  "latitude" : "lat 2",
+  "longitude" : "long 1",
+  "username" : "ABCD"
+}
